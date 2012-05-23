2205c1589f632631f1c63126340fc1ff63e7f315
renatodex <renatodex@gmail.com>
2012-05-23 00:53:19 -0300

    Cleaning app/pages

--- Spell/app/pages/Page1.js
+++ Spell/app/pages/Page1.js
@@ -1,36 +1,10 @@
 define(['lib/pages', 'lib/draw', 'lib/event'], function(Page, Draw, Event) {
     return Page.extend({ 
         setup : function() {
-            SpellStorage.Animation.enable();
-            SpellStorage.Objects.wipe();
-            this.caixa1 = Draw.Square('caixa1', {
-                x: 50,
-                y: 50,
-                width: 100,
-                height: 100
-            });
-
-            this.variavel = '123';
+            
         },
         draw : function() {
             
-            var c1 = this.caixa1;
-            
-            if(this.caixa1.getX() <= 300) {
-                c1.addX(10);
-            } else {
-                if(this.caixa1.getY() >= 500) {
-                    c1.addX(-10);
-                } else {
-                    c1.addY(10);
-                }
-            }
-            
-            c1.render();
-            
-        },
-        stage_down : function() {
-            console.log(this.variavel);
         }
     });
 });
--- Spell/app/pages/Page2.js
+++
@@ -1,9 +0,0 @@
-define(['lib/page'], function(Page) {
-   var Page2 = Page.extend({
-       init : function() {
-           
-       }
-   });
-   
-   return Page2;
-});
